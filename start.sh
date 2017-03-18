4a9dd8294b2629c42985cce24ae07a9475624321
kongo2002 <kongo2002@googlemail.com>
2017-03-18 17:07:43 +0100

    move log level into app.config

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec erl -env ERL_LIBS _build/default/lib -eval 'application:ensure_all_started(statser).' -noshell
+exec erl -env ERL_LIBS _build/default/lib -config app.config -eval 'application:ensure_all_started(statser).' -noshell
