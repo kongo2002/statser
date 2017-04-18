22ccebebb328bfc267b3598fde97587201e759bf
kongo2002 <kongo2002@googlemail.com>
2017-04-18 23:39:16 +0200

    use elli_request:post_args_decoded instead

--- src/statser_api.erl
+++ src/statser_api.erl
@@ -7,10 +7,51 @@
 handle(Req, _Args) ->
     handle(Req#req.method, elli_request:path(Req), Req).
 
+
+% render API
+handle('POST', [<<"render">> | _], Req) ->
+    Args = elli_request:post_args_decoded(Req),
+    Targets = many_by_key(<<"target">>, Args),
+    From = get_by_key(<<"from">>, Args),
+    Until = get_by_key(<<"until">>, Args),
+    MaxPoints = get_or_fallback(<<"maxDataPoints">>, Args, 366),
+
+    handle_render(Targets, From, Until, MaxPoints);
+
 handle(_, _, _Req) ->
     {404, [], <<"not found">>}.
+
 
 %% @doc: Handle request events, like request completed, exception
 %% thrown, client timeout, etc. Must return 'ok'.
 handle_event(_Event, _Data, _Args) ->
     ok.
+
+
+handle_render([], _From, _Until, _MaxPoints) ->
+    {400, [], <<"no target(s) specified">>};
+handle_render(_Targets, false, _Until, _MaxPoints) ->
+    {400, [], <<"no 'from' specified">>};
+handle_render(_Targets, _From, false, _MaxPoints) ->
+    {400, [], <<"no 'until' specified">>};
+handle_render(Targets, From, Until, MaxPoints) ->
+    lager:debug("render request ~p [~p - ~p] [~w]", [Targets, From, Until, MaxPoints]),
+    {ok, [], <<"not implemented yet">>}.
+
+
+many_by_key(Key, List) ->
+    many_by_key(Key, List, []).
+
+many_by_key(_Key, [], Acc) -> Acc;
+many_by_key(Key, [{Key, Value} | Rest], Acc) ->
+    many_by_key(Key, Rest, [Value | Acc]);
+many_by_key(Key, [_Otherwise | Rest], Acc) ->
+    many_by_key(Key, Rest, Acc).
+
+get_or_fallback(Key, List, Default) ->
+    case lists:keyfind(Key, 1, List) of
+        false -> Default;
+        {Key, Value} -> Value
+    end.
+
+get_by_key(Key, List) -> get_or_fallback(Key, List, false).
