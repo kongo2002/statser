e69b5e6b27193e5ccd8d56e5b527841c205395c5
kongo2002 <kongo2002@googlemail.com>
2017-05-09 21:25:29 +0200

    add aliasByMetric and aliasByNode

--- src/statser_processor.erl
+++ src/statser_processor.erl
@@ -33,6 +33,55 @@ fetch_data(Paths, From, Until, Now) ->
 evaluate_call(<<"alias">>, [Series, Alias], _From, _Until, _Now) when is_binary(Alias) ->
     lists:map(fun({_Target, Values}) -> {Alias, Values} end, Series);
 
+% aliasByMetric
+evaluate_call(<<"aliasByMetric">>, [Series], _From, _Until, _Now) ->
+    lists:map(fun({Target, Values}) ->
+                      {alias_target(Target, [-1]), Values}
+              end, Series);
+
+% aliasByNode
+evaluate_call(<<"aliasByNode">>, [Series | Aliases], _From, _Until, _Now) ->
+    lists:map(fun({Target, Values}) ->
+                      {alias_target(Target, Aliases), Values}
+              end, Series);
+
 evaluate_call(Unknown, _Args, _From, _Until, _Now) ->
     lager:error("unknown function call ~p or invalid arguments", [Unknown]),
     error.
+
+
+alias_target(Target, []) -> Target;
+alias_target(Target, Aliases) ->
+    Parts = binary:split(Target, <<".">>, [global, trim_all]),
+    NumParts = length(Parts),
+    to_target(lists:map(fun(Idx) -> get_part(Parts, NumParts, Idx) end, Aliases)).
+
+
+get_part(Parts, _Length, Idx) when Idx >= 0 ->
+    lists:nth(Idx + 1, Parts);
+get_part(Parts, Length, Idx) ->
+    lists:nth(Length + Idx + 1, Parts).
+
+
+to_target(Parts) ->
+    F = fun(A, <<>>) -> <<A/binary>>;
+           (A, B) -> <<A/binary, ".", B/binary>>
+        end,
+    lists:foldr(F, <<>>, Parts).
+
+
+%%
+%% TESTS
+%%
+
+-ifdef(TEST).
+
+alias_target_test_() ->
+    [?_assertEqual(<<"foo">>, alias_target(<<"foo">>, [])),
+     ?_assertEqual(<<"foo.bar">>, alias_target(<<"foo.bar">>, [])),
+     ?_assertEqual(<<"test">>, alias_target(<<"foo.bar.test">>, [2])),
+     ?_assertEqual(<<"test">>, alias_target(<<"foo.bar.test">>, [-1])),
+     ?_assertEqual(<<"foo">>, alias_target(<<"foo.bar.test">>, [0]))
+    ].
+
+-endif.
