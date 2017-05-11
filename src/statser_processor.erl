4e9ec3e3f750974a7b87c460cf683139862e4224
kongo2002 <kongo2002@googlemail.com>
2017-05-11 21:11:52 +0200

    add averageAbove and averageBelow

--- src/statser_processor.erl
+++ src/statser_processor.erl
@@ -51,6 +51,14 @@ evaluate_call(<<"aliasByNode">>, [Series | Aliases], _From, _Until, _Now) ->
                       {alias_target(Target, Aliases), Values}
               end, Series);
 
+% averageAbove
+evaluate_call(<<"averageAbove">>, [Series, Avg], _From, _Until, _Now) ->
+    lists:filter(fun({_, Values}) -> safe_average(Values) >= Avg end, Series);
+
+% averageBelow
+evaluate_call(<<"averageBelow">>, [Series, Avg], _From, _Until, _Now) ->
+    lists:filter(fun({_, Values}) -> safe_average(Values) =< Avg end, Series);
+
 evaluate_call(Unknown, _Args, _From, _Until, _Now) ->
     lager:error("unknown function call ~p or invalid arguments", [Unknown]),
     error.
@@ -82,6 +90,22 @@ to_target(Parts) ->
     lists:foldr(F, <<>>, Parts).
 
 
+safe_average(Values) ->
+    safe_average(Values, 1, 0.0).
+
+safe_average([], _Cnt, Avg) -> Avg;
+safe_average([{_TS, null} | Vs], Cnt, Avg) ->
+    safe_average(Vs, Cnt, Avg);
+safe_average([{_TS, Val} | Vs], Cnt, Avg) ->
+    NewAvg = Avg + (Val - Avg) / Cnt,
+    safe_average(Vs, Cnt + 1, NewAvg);
+safe_average([null | Vs], Cnt, Avg) ->
+    safe_average(Vs, Cnt, Avg);
+safe_average([Val | Vs], Cnt, Avg) ->
+    NewAvg = Avg + (Val - Avg) / Cnt,
+    safe_average(Vs, Cnt + 1, NewAvg).
+
+
 %%
 %% TESTS
 %%
@@ -94,6 +118,31 @@ alias_target_test_() ->
      ?_assertEqual(<<"test">>, alias_target(<<"foo.bar.test">>, [2])),
      ?_assertEqual(<<"test">>, alias_target(<<"foo.bar.test">>, [-1])),
      ?_assertEqual(<<"foo">>, alias_target(<<"foo.bar.test">>, [0]))
+    ].
+
+safe_average_test_() ->
+    [?_assertEqual(4.0, safe_average([4])),
+     ?_assertEqual(3.0, safe_average([2, 4])),
+     ?_assertEqual(3.0, safe_average([2, 4, 4, 2])),
+     ?_assertEqual(0.0, safe_average([])),
+     ?_assertEqual(0.0, safe_average([null])),
+     ?_assertEqual(3.0, safe_average([2, 4, null])),
+     ?_assertEqual(4.0, safe_average([null, 4, null])),
+     ?_assertEqual(3.0, safe_average([{0, 2}, {0, 4}, {0, null}])),
+     ?_assertEqual(4.0, safe_average([{0, null}, {0, 4}, {0, null}]))
+    ].
+
+average_above_test_() ->
+    Series = [{<<"target">>, [{100, 5.0}, {110, 7.0}]}],
+    [?_assertEqual(Series, evaluate_call(<<"averageAbove">>, [Series, 6.0], 0, 0, 0)),
+     ?_assertEqual([], evaluate_call(<<"averageAbove">>, [Series, 6.5], 0, 0, 0))
+    ].
+
+average_below_test_() ->
+    Series = [{<<"target">>, [{100, 5.0}, {110, 7.0}]}],
+    [?_assertEqual([], evaluate_call(<<"averageBelow">>, [Series, 5.5], 0, 0, 0)),
+     ?_assertEqual(Series, evaluate_call(<<"averageBelow">>, [Series, 6.0], 0, 0, 0)),
+     ?_assertEqual(Series, evaluate_call(<<"averageBelow">>, [Series, 10], 0, 0, 0))
     ].
 
 -endif.
