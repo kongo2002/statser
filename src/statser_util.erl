7e63fd21332f3c8a380deaa6b2deb1048df08e19
kongo2002 <kongo2002@googlemail.com>
2017-05-21 23:53:57 +0200

    add tests for floor and ceiling

--- src/statser_util.erl
+++ src/statser_util.erl
@@ -28,3 +28,28 @@ ceiling(X) ->
        true -> Truncated;
        false -> Truncated + 1
     end.
+
+%%
+%% TESTS
+%%
+
+-ifdef(TEST).
+
+floor_test_() ->
+    [?_assertEqual(5, floor(5.0)),
+     ?_assertEqual(5, floor(5.5)),
+     ?_assertEqual(5, floor(5.9)),
+     ?_assertEqual(-6, floor(-5.1)),
+     ?_assertEqual(-6, floor(-5.9))
+    ].
+
+ceiling_test_() ->
+    [?_assertEqual(5, ceiling(5.0)),
+     ?_assertEqual(5, ceiling(5)),
+     ?_assertEqual(6, ceiling(5.5)),
+     ?_assertEqual(6, ceiling(5.9)),
+     ?_assertEqual(-5, ceiling(-5.1)),
+     ?_assertEqual(-5, ceiling(-5.9))
+    ].
+
+-endif.
