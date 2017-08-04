9243fde5db6309857d90bd834abcede85e34f709
kongo2002 <kongo2002@googlemail.com>
2017-08-04 22:25:05 +0200

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec erl +B -env ERL_LIBS _build/default/lib -config app.config -eval 'application:ensure_all_started(statser).' -noinput
+exec erl +B -env ERL_LIBS _build/default/lib -config config/sys.config -setcookie statser -sname statser -eval 'application:ensure_all_started(statser).' -noinput
